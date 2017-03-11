a132f60e5fad7edb17d0d7f3cbf8193ed150ec84
Sung Choi <m.sung.choi@gmail.com>
2017-03-10 17:17:19 -0800

    Updates formatDecileGenerator function. isToppedOut property is currently not doing anything. Although it's not currently problematic, I'm realizing I'm not totally sure what isToppedOut means and whether it affects benchmarks like the one with measureId (qualityId) of 109 and submissionMethod of Registry/QCDR.

--- util/format-decile-generator.js
+++ util/format-decile-generator.js
@@ -21,9 +21,27 @@
 var formatDecileGenerator = function formatDecileGenerator(record) {
   var range = record.decile3.match(/(\d{0,3}\.?\d{2,})/g);
   var isInverseMeasure = range && range[1] && range[0] - range[1] > 0;
-  var top = null;
-  if (record.isToppedOut) top = isInverseMeasure ? 0 : 100;
-
+  /** NOTE: TODO(sung):
+   * Is the toppedOut property meaningful?
+   * What should the 9th decile be in this case:
+   * qualityId: 109
+   * submissionMethod: qcdrOrQualifiedRegistry (Registry/QCDR)
+   * decile3: 5.16 - 14.84
+   * decile4: 14.85 - 37.78
+   * decile5: 37.79 - 65.33
+   * decile6: 65.34 - 88.04
+   * decile7: 88.05 - 97.81
+   * decile8: 97.82 - 99.99
+   * decile9: --
+   * decile10: 100
+   * isToppedOut: No
+   *
+   * Seems like it should be 100 as it is now.
+   * Does it just mean that it is not possible to get a score greater than 100?
+   */
+  // var top = null;
+  // if (record.isToppedOut.trim().toLowerCase() === 'yes') top = isInverseMeasure ? 0 : 100;
+  var top = isInverseMeasure ? 0 : 100;
   /**
    * @param {string | null} decileString - from csv
    * @param {number} index
