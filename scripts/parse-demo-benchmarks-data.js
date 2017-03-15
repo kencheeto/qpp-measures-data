f4864dc6b75ba8775a64ed85082ee5226b5cce07
Sung Choi <m.sung.choi@gmail.com>
2017-03-14 21:48:02 -0400

    Updates parse-demo-benchmarks-data.js script to use qualityIds as keys again in order to prevent benchmarks being lost in the process.

--- scripts/parse-demo-benchmarks-data.js
+++ scripts/parse-demo-benchmarks-data.js
@@ -1,17 +1,16 @@
-// Script to generate benchmark.json file from csv
 // Libraries
 var fs    = require('fs');
 var parse = require('csv-parse');
 var path  = require('path');
 // Constants
 var DEMO_CSV_COLUMNS          = require('./../util/constants/demo-csv-columns');
-var QUALITY_ID_TO_MEASURE_MAP = require('./../util/constants/quality-id-to-measure-map');
 // Utils
 var formatCsvRecord         = require('./../util/format-csv-record');
 var formatSubmissionMethod  = require('./../util/format-submission-method');
 // Data
 var benchmarksData  = fs.readFileSync(path.join(__dirname, './../data/historical-benchmarks/demo.csv'), 'utf8');
 
+// Script to generate benchmark.json file from csv
 var benchmarks = {};
 
 parse(benchmarksData, {columns: DEMO_CSV_COLUMNS, from: 4}, function(err, records) {
@@ -19,14 +18,12 @@ parse(benchmarksData, {columns: DEMO_CSV_COLUMNS, from: 4}, function(err, record
     console.log(err);
   } else {
     records.forEach(function(record) {
-      var measure = QUALITY_ID_TO_MEASURE_MAP[record.qualityId];
+      var qualityId = record.qualityId;
 
-      if (measure && measure.measureId) {
-        var measureId = measure.measureId;
-
-        if (!benchmarks[measureId]) benchmarks[measureId] = {};
-        benchmarks[measureId][formatSubmissionMethod(record.submissionMethod)] = formatCsvRecord(record, {benchmarkYear: '2016', performanceYear: '2018'});
-      }
+      // NOTE: qualityId is being used here since not all benchmarks currently have corresponding measures.
+      // This will prevent the loss of benchmarks during parsing.
+      if (!benchmarks[qualityId]) benchmarks[record.qualityId] = {};
+      benchmarks[qualityId][formatSubmissionMethod(record.submissionMethod)] = formatCsvRecord(record, {benchmarkYear: '2016', performanceYear: '2018'});
     });
 
     fs.writeFileSync(path.join(__dirname, '../benchmarks/demo.json'), JSON.stringify(benchmarks), 'utf8');
